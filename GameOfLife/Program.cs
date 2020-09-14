50e8b7629564a4cc7bf165003e82015ea81fc978
Renate Tomilova <renate.tomilova@accenture.com>
2020-09-14 09:07:38 +0300

    New generation creation fixed. Naming fixed.

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -8,36 +8,47 @@ namespace GameOfLife
         private static void Main(string[] args)
         {
 
-            Console.Write("Enter board width: ");
-            int width = int.Parse(Console.ReadLine());
-            if (width <= 0 || width > 50)
-            {
-                Console.WriteLine("Entered number is out of range. Please enter a number from 1 to 50.");
-            }
-
+             Console.Write("Enter board width: ");
+             int width;
+             while (!int.TryParse(Console.ReadLine(), out width))
+             {
+                 Console.WriteLine("Entered value is not numeric. Please enter number from 1 to 50: ");
+             }
+             if (width <= 0 || width > 50)
+             {
+                 Console.WriteLine("Entered number is out of range. Please enter a number from 1 to 50.");
+             }
+              
             Console.Write("Enter board heigth: ");
-            int heigth = int.Parse(Console.ReadLine());
-            if (heigth <= 0 || width > 50)
+            int heigth;
+            while (!int.TryParse(Console.ReadLine(), out heigth))
+            {
+                Console.WriteLine("Entered value is not numeric.");
+            }
+            if (heigth <= 0 || heigth > 50)
             {
                 Console.WriteLine("Entered number is out of range. Please enter a number from 1 to 50.");
             }
             //check entered value
-            //check entered number
+            //check entered number -> Repeat asking to enter the same parametr till number is in the range
 
             Game session = new Game(heigth, width);
 
-            session.StartGame();
+           // session.StartGame();
 
-            int gen = 0;        // will be used for generation counting
-
+            int gen = 0;
             while (gen++ < 100)
             {
-                session.FullCycle();
+                session.ExecuteFullCycle();
                 Console.WriteLine();
                 Console.WriteLine("Generation: " + gen);
-                Console.Write("Alive cells: " );
+                session.CountNumberOfAliveCells();
                 System.Threading.Thread.Sleep(1000);
+                //while (Console.ReadKey().Key != ConsoleKey.Enter) { } // added to check if neighbour are counting correctly. will be removed.
+                
             }
+
+            Console.ReadLine();
         }
     }
 }
