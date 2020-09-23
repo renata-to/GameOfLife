9fe1dcbdf8ce8059d2bd6b1025c094e9c38f8c43
Renate Tomilova <renate.tomilova@accenture.com>
2020-09-23 16:36:33 +0300

    Improved structure. Almost done serialization.

---
+++ GameOfLife/ConsoleManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife
+{
+    public class ConsoleManager
+    {
+        /// <summary>
+        /// Prints out welcome message
+        /// </summary>
+        public void WelcomeToTheGame()
+        {
+            Console.WriteLine("<><><><><><><><>><><><>><><>");
+            Console.WriteLine("Welcome to Game of Life.");
+            Console.WriteLine("<><><><><><><><>><><><>><><>");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Shows main menu to select from start, load, exit.
+        /// </summary>
+        public void ShowGameMenu()
+        {
+            Console.WriteLine("To start a new game please enter 1.");
+            Console.WriteLine("To load previous game please enter 2.");
+            Console.WriteLine("To exit game please enter 3.");
+        }
+
+        /// <summary>
+        /// Prints out a board
+        /// </summary>
+        public void PrintBoard(GameLogic game)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            for (int x = 0; x < game.width; x++)
+            {
+                for (int y = 0; y < game.heigth; y++)
+                {
+                    Console.Write(game.NowGeneration[x, y] ? "O" : " ");
+                }
+                Console.WriteLine();
+            }
+            //Console.SetCursorPosition(0, Console.WindowTop);
+        }
+    }
+}
